e875fc9d38c4c86bd4cb52317729c1d137503b7a
davi lima <davirasenga@gmail.com>
2024-01-25 20:57:37 -0300

    Primeiro commit de muitos

---
+++ src/main/java/com/davidev/RegUserSecureAutRecoverPasw/RegUserSecureAutRecoverPaswApplication.java
@@ -0,0 +1,13 @@
+package com.davidev.RegUserSecureAutRecoverPasw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegUserSecureAutRecoverPaswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegUserSecureAutRecoverPaswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidev/RegUserSecureAutRecoverPasw/RegUserSecureAutRecoverPaswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidev.RegUserSecureAutRecoverPasw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegUserSecureAutRecoverPaswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
